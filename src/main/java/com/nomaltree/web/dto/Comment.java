182ec16c943ef34aafe9848322cf856d808cec75
Choi, Sungho <gfhyrfgjh@gmail.com>
2024-05-23 16:45:21 +0900

    사이드바 디자인 구현
    적성검사 사이드바 디자인 구현 완료

--- src/main/java/com/nomaltree/web/dto/Comment.java
+++ src/main/java/com/nomaltree/web/dto/Comment.java
@@ -2,14 +2,21 @@ package com.nomaltree.web.dto;
 
 import java.util.Date;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 @Data
 public class Comment {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	int id;
 	String content;
 	Date regdate;
 	String strRegDate;
 	int noticeId;
 	String nickname;
-	int id;
+
 }
--- src/main/java/com/nomaltree/web/dto/User.java
+++ src/main/java/com/nomaltree/web/dto/User.java
@@ -1,10 +1,12 @@
 package com.nomaltree.web.dto;
 
+import jakarta.persistence.Id;
 import lombok.Data;
 
 @Data
 public class User {
 
+	@Id
 	String id;
 	String nickname;
 	String password;
